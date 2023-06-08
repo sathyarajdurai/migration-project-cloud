0f579d702d65df0ceea3adb3b90c37cf86e0b26c
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2023-06-08 20:17:20 +0530

    delete vpc peer

--- vpcpeer/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "cloud_cidr" {
-  description = "this is cloud cidr"
-  type        = string
-}
-
-variable "onprem_cidr" {
-  description = "this is on-prem cidr"
-  type        = string
-}
